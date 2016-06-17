82563ca63422c0c698d1e97cad74bf1efc06d56c
itsvetkoff <tsvetkoff@i.ua>
2016-06-17 13:43:21 +0300

    solutions file with explanations

--- solutions.js
+++ solutions.js
@@ -2,6 +2,23 @@ var solutions = {};
 
 solutions.andreiZubrytskyi = function (board) {
     // YOUR SOLUTION GOES HERE
+    // board: 2-d array, where
+    // -1  - wall
+    //  0  - emptry cell
+    // 's' - start
+    // 'f' - finish
+
+    // var board = [
+    //     ['s', -1,  -1,   0,  0  ],
+    //     [0,    0,  -1,  -1,  0  ],
+    //     [0,    0,  -1,  -1,  0  ],
+    //     [0,    0,   0,  -1,  0  ],
+    //     [0,    0,   0,   0, 'f' ]
+    // ]
+
+    // expected: path array
+    // return [ [0,0], [1,2], [0,4], [2, 3], [4,4] ]
+    // if s в точке (0,0), f в точке (2,4)
 };
 
 solutions.bohdanVolyk = function (board) {
