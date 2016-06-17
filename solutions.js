473b36b3fd4c92cc8415581cc35a2fec2698c67a
itsvetkoff <tsvetkoff@i.ua>
2016-06-17 03:07:07 +0300

    solutions file

---
+++ solutions.js
@@ -0,0 +1,39 @@
+var solutions = {};
+
+solutions.andreiZubrytskyi = function (board) {
+    // YOUR SOLUTION GOES HERE
+};
+
+solutions.bohdanVolyk = function (board) {
+    // YOUR SOLUTION GOES HERE
+};
+
+solutions.katerynaMazurkevych = function (board) {
+    // YOUR SOLUTION GOES HERE
+};
+
+solutions.liudmylaPolianychko = function (board) {
+    // YOUR SOLUTION GOES HERE
+};
+
+solutions.maksymYurchenko = function (board) {
+    // YOUR SOLUTION GOES HERE
+};
+
+solutions.olhaRomankiv = function (board) {
+    // YOUR SOLUTION GOES HERE
+};
+
+solutions.ostapKhomitskyi = function (board) {
+    // YOUR SOLUTION GOES HERE
+};
+
+solutions.volodymyrPantasenko = function (board) {
+    // YOUR SOLUTION GOES HERE
+};
+
+solutions.yaroslavSamoilenko = function (board) {
+    // YOUR SOLUTION GOES HERE
+};
+
+module.exports = solutions;
