12447a3214e8604ff4cec80605bff2a5f1b1a3ca
Andrey Zubritskiy <andreizubritskiy@gmail.com>
2016-06-20 10:48:23 +0300

    Task is completed

--- solutions.js
+++ solutions.js
@@ -1,24 +1,77 @@
 var solutions = {};
 
 solutions.andreiZubrytskyi = function (board) {
-    // YOUR SOLUTION GOES HERE
-    // board: 2-d array, where
-    // -1  - wall
-    //  0  - emptry cell
-    // 's' - start
-    // 'f' - finish
+    let first_x, first_y, last_x, last_y;
 
-    // var board = [
-    //     ['s', -1,  -1,   0,  0  ],
-    //     [0,    0,  -1,  -1,  0  ],
-    //     [0,    0,  -1,  -1,  0  ],
-    //     [0,    0,   0,  -1,  0  ],
-    //     [0,    0,   0,   0, 'f' ]
-    // ]
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] === 's') {
+                first_x = j;
+                first_y = i;
+            }
+            else if (board[i][j] === 'f') {
+                last_x = i;
+                last_y = j;
+            }
+        }
+    }
+    // possible steps
+    let x = [1, 1, -1, -1, 2, 2, -2, -2];
+    let y = [2, -2, 2, -2, 1, -1, 1, -1];
 
-    // expected: path array
-    // return [ [0,0], [1,2], [0,4], [2, 3], [4,4] ]
-    // if s в точке (0,0), f в точке (2,4)
+    let queue = [];
+
+    queue.push([first_x, first_y]);
+
+    let used = [];
+    for (let i = 0; i < board.length; i++) {
+        used[i] = [];
+    }
+    used[first_x][first_y] = true;
+
+    let parent = [];
+    for (let i = 0; i < board.length; i++) {
+        parent[i] = [];
+    }
+
+    // breadth-first search
+    while (queue.length > 0) {
+        let vertex = queue.shift();
+        for (let i = 0; i < 8; i++) {
+            let vx = vertex[0] + x[i];
+            let vy = vertex[1] + y[i];
+
+            if (vx >= 0 && vy >= 0 && vx < board.length && vy < board[0].length && board[vx][vy] !== -1) {
+                if (!used[vx][vy]) {
+                    used[vx][vy] = true;
+                    queue.push([vx, vy]);
+                    parent[vx][vy] = vertex;
+                }
+            }
+        }
+    }
+
+    // the shortest path
+    let path = [];
+
+    let i = last_x, j = last_y;
+    path.push([last_x, last_y]);
+
+    while (i != first_x && j != first_y) {
+
+        if (typeof parent[i][j] != 'undefined') {
+            let vx = parent[i][j][0];
+            j = parent[i][j][1];
+            i = vx;
+            path.push([i, j]);
+        }
+        else {
+            return [];
+        }
+    }
+    path.reverse();
+
+    return path;
 };
 
 solutions.bohdanVolyk = function (board) {
